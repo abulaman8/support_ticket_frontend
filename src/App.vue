9ae777a38dbae2ff0690ede335309bc1c2e061a8
5.413U14M4N <abulaman6@gmail.com>
2023-04-16 13:21:57 +0530

    almost done, not really

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  </nav> -->
+  <!-- <TopNav></TopNav> -->
+  <SideNav></SideNav>
+  <TopNav></TopNav>
+  <div class="main"><router-view/></div>
 </template>
 
+<script>
+import SideNav from './components/SideNav.vue';
+import TopNav from './components/TopNav.vue';
+  export default {
+    components: {
+      SideNav,
+      TopNav
+    }
+  }
+
+</script>
+
 <style>
+/* *{
+  outline: limegreen 1px solid;
+} */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 0;
+  padding: 0;
+
 }
 
-nav {
-  padding: 30px;
+.main{
+  margin-top: 100px;
+  margin-left: 80px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+
+
 </style>
